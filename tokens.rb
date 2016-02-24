c4907ba7e2e37af87dddfb798835e19efa60c032
S Abhishek Anand <anandab@Vulcan3.cis.uab.edu>
2016-02-23 23:34:08 -0600

    modified token

--- tokens.rb
+++ tokens.rb
@@ -2,42 +2,44 @@ class Token
   T_EOF = 0
   T_SEMICOLON = 1
   T_IDENT = 2
-  T_EQUAL = 3
+  T_RELOP = 3
   T_IF = 4
   T_THEN = 5
   T_ELSE = 6
   T_WHILE = 7
   T_DO = 8
   T_END = 9
-  T_PLUS = 10
-  T_MINUS = 11
-  T_MULT = 12
-  T_DIVIDE = 13
+  T_ADDOP = 10
+  T_MULOP = 11
+  T_ASN = 12
+  T_COMMENT = 13
   T_INTEGER = 14
   T_LEFT_PAREN = 15
   T_RIGHT_PAREN = 16
-  T_TRUE = 17
-  T_FALSE = 18
+  T_BOOLEAN = 17
 end
 
 $keywords = {
   "eof" => Token::T_EOF,
   ";" => Token::T_SEMICOLON,
   "id" => Token::T_IDENT,
-  "=" => Token::T_EQUAL,
+  "=" => Token::T_RELOP,
+  "<" => Token::T_RELOP,
+  "<=" => Token::T_RELOP,
   "if" => Token::T_IF,
   "then" => Token::T_THEN,
   "else" => Token::T_ELSE,
   "while" => Token::T_WHILE,
   "do" => Token::T_DO,
   "end" => Token::T_END,
-  "+" => Token::T_PLUS,
-  "-" => Token::T_MINUS,
-  "*" => Token::T_MULT,
-  "/" => Token::T_DIVIDE,
+  "+" => Token::T_ADDOP,
+  "-" => Token::T_ADDOP,
+  "*" => Token::T_MULOP,
+  "/" => Token::T_MULOP,
   "int" => Token::T_INTEGER,
   "(" => Token::T_LEFT_PAREN,
   ")" =>Token::T_RIGHT_PAREN,
-  "true" => Token::T_TRUE,
-  "false" => Token::T_FALSE
+  "true" => Token::T_BOOLEAN,
+  "false" => Token::T_BOOLEAN,
+  ":=" => Token::T_ASN
 }
