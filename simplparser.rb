e75b10481d5aefeca8ad638e7aa5c81061152798
Sujan Khadka <sujank@uab.edu>
2016-02-26 00:49:49 -0600

    added simplparser.rb which integrates our lexer and parser

---
+++ simplparser.rb
@@ -0,0 +1,20 @@
+require_relative './lexer'
+require_relative './parser'
+
+#TODO: create Tokenizer object
+
+def getTokenKind()
+  #TODO: call tokenizer method
+end
+
+def getTokenText()
+  #TODO: call tokenizer method
+end
+
+#consume token
+def nextToken()
+  #TODO: call tokenizer method
+end
+
+#start parser program
+parseProgram()
